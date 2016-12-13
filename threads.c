2066a6f7f39621ac4786242ca744e0dc6a8fbb29
NikitaPri <NikitasPri@users.noreply.github.com>
2016-12-13 14:38:11 +0300

    char

--- threads.c
+++ threads.c
@@ -10,12 +10,39 @@
 #include <unistd.h>
 int variable, fd;
 
-int func()
+int func(int argc)
 {
 	variable=42;
 	char str[]="child thread is working";
 	int len=strlen(str);
 	write(1, str, len);
+	
+	int n;
+	n=argc;
+	//convert int to char
+	char* c;
+	c=(char *)malloc(10 * sizeof(char));
+	int v=0;
+	while (n > 9)
+	{
+		c[v++]=(n%10)+'0';
+		n=n/10;
+	}
+	c[v++]=n+'0';
+	c[v]='\0';
+	char t;
+	int i=0;
+	for (i=0; i<v/2;i++)
+	{
+		t=c[i];
+		c[i]=c[v-1-i];
+		c[v-1-i]=t;
+	}
+	write(1, c, strlen(c));
+	free(c);
+	
+	char str1[]="child thread number 2";
+	write(1, str1, strlen(str1));
 	_exit(0);
 }
 
@@ -29,8 +56,8 @@ int main(int argc, char *argv[])
 	child_stack += 16384 -1;
 	printf("The variable was %d\n", variable);
 	char str[]="main thread is working";
-        write(1, str, strlen(str));
-	clone(func, child_stack, CLONE_VM, NULL);
+        
+	clone(func, child_stack, CLONE_VM, 123);
 	sleep(1);
 	printf("variable is %d\n", variable);
 	return 0;
