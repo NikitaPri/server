4a231c0a161ee762909301164c383cd16e38e7d3
NikitaPri <NikitasPri@users.noreply.github.com>
2016-12-15 15:18:17 +0300

    threads.c

--- threads.c
+++ threads.c
@@ -24,8 +24,8 @@ int *Plan_matrix;
 int func(int argc, int agrv)
 {
 	//variable=42;
-	char str[]="child thread with id ";
-	char newline[]="\n";	
+	//char str[]="child thread with id ";
+	//char newline[]="\n";	
 	int n;
 	n=argc-1;
 	/*int *A[N][N];
@@ -106,7 +106,7 @@ int planner(int argc,int argv[][NN]){
 			argv[i][j]=0;
 		}
 	}
-	printf("is n %d\n", n);
+	/*printf("is n %d\n", n);
 	for (i=0; i<N*N; i++)
 	{
 		id=i%n;	
@@ -114,7 +114,7 @@ int planner(int argc,int argv[][NN]){
 		temp=argv[id][0];
 		argv[id][temp]=i;
 		printf("Current state: id -- %d, argv[id][0] -- %d, i -- %d\n", id, temp,i);
-	}
+	}*/
 	return 0;
 }
 
@@ -139,7 +139,6 @@ int main(int argc, char *argv[]){
 	Plan_matrix=Plan[0];
 	int c=0;
 	int s=0;
-	printf("first token is %d\n", N);
 	/*while(token!=NULL)	
 	{
 		printf("%s\n", token);
@@ -232,6 +231,7 @@ int main(int argc, char *argv[]){
 		}
 	}
 	*/
+	/*
 	printf("matrix A\n");
 	for (i=0; i<N; i++)
 	{
@@ -259,7 +259,7 @@ int main(int argc, char *argv[]){
                         printf("%d    ", C[i][j]);
                 }
                 printf("\n");
-        }
+        }*/
 	int elem;
 	int stroka;
 	int stolbec;
@@ -274,29 +274,35 @@ int main(int argc, char *argv[]){
 	long stacksize=1024;
 	void *stack;
 	//stack=malloc(stacksize);
-
-	printf("*A_Matrix[2][1] is %d, *B_matrix[2][2] is %d\n", *(A_matrix+2*N+1), *(B_matrix+2*N+2));	
-		
+	double time[N*N][2];
 	//n_threads=9;
 	for (n_threads=1;n_threads<=N*N;n_threads++)
 	{
+		for (i=0; i<N; i++)
+		{
+			for (j=0;j<N; j++)
+			{
+				C[i][j]=0;
+			}
+		}
 		planner(n_threads, Plan);
-		printf("Tasks\n");
-		for (i=0;i<NN;i++)
+		
+//printf("Tasks\n");
+	/*	for (i=0;i<NN;i++)
 		{
 			for (j=0;j<NN;j++)
 			{
 				printf("%d     ", Plan[i][j]);
 			}
 		printf("\n");
-		}
+		}*/
 		clock_gettime(CLOCK_MONOTONIC, &tstart);
 		for (i=1; i<=n_threads; i++)
 		{
 			stack=malloc(stacksize*i);
 			pid=clone(func, stack+stacksize, CLONE_VM | SIGCHLD, i, stacksize);
 		
-			printf("Created child thread with id %d\n", i);
+			//printf("Created child thread with id %d\n", i);
 		
 		}
 		for (i=1; i<=n_threads; i++)
@@ -304,16 +310,39 @@ int main(int argc, char *argv[]){
 			wait(NULL);
 		}
 		clock_gettime(CLOCK_MONOTONIC, &tend);
-		printf("time is %.5f\n", ((double)tend.tv_sec+1.0e-9*tend.tv_nsec)-((double)tstart.tv_sec+1.0e-9*tstart.tv_nsec));
-		printf("matrix C\n");
-                for (i=0; i<N; i++)
+		time[n_threads][0]=n_threads;
+		time[n_threads][1]=((((double)tend.tv_sec+1.0e-9*tend.tv_nsec)-((double)tstart.tv_sec+1.0e-9*tstart.tv_nsec)));
+		//printf("time for n_threads %d is %.5f\n",n_threads, time[n_threads][1]);
+		//printf("matrix C\n");
+                /*for (i=0; i<N; i++)
         	{
                 	for (j=0; j<N; j++)
                 	{
                         	printf("%d    ", C[i][j]);
                 	}
                 	printf("\n");
-        	}
+        	}*/
 	}
+	double min_time=0;
+	min_time=time[1][0];
+	int threads=0;
+	threads=1;
+	/*for (i=0;i<N*N; i++)
+	{
+		printf("n_threads %d -- time %.5f\n", i, time[i][1]);
+	}*/
+	for (i=2; i<N*N; i++)
+	{	
+		if (time[i][1]<min_time)
+		{
+			min_time=time[i][1];
+			threads=time[i][0];
+		}
+		else
+		{
+		}
+	//printf("threads is %d\n", threads);
+	}
+	printf("%d\n", threads);
 	return 0;
 }
